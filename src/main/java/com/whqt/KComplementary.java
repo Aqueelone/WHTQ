e1607605d269a724dd7644e767af05da09b78f51
Sergy Nazarevich <aqueelone@gmail.com>
2016-08-11 16:44:14 +0300

    add KComplimentary.Class for solve problem add KComplimentaryTest for testing, but "in red" now

---
+++ src/main/java/com/whqt/KComplementary.java
@@ -0,0 +1,11 @@
+package com.whqt;
+
+/**
+ *
+ * @author Sergy Nazarevich
+ */
+public class KComplementary {
+    public static int countComplimentary( int K, int[ ] A ){
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
