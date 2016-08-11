82d551ef8bd7ada3f785159d0ce2c619ce5dad3b
Sergy Nazarevich <aqueelone@gmail.com>
2016-08-11 17:48:40 +0300

    refactored KComplimentary.Class and test for it for tuning task target, "in red"

--- src/main/java/com/whqt/KComplementary.java
+++ src/main/java/com/whqt/KComplementary.java
@@ -1,11 +1,13 @@
 package com.whqt;
 
+import java.util.Map;
+
 /**
  *
  * @author Sergy Nazarevich
  */
 public class KComplementary {
-    public static int countComplimentary( int K, int[ ] A ){
+    public static Map<Integer, Integer> findComplimentary( Integer K, Integer[] A ){
                 throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
--- src/test/java/com/whqt/KComplementaryTest.java
+++ src/test/java/com/whqt/KComplementaryTest.java
@@ -5,6 +5,9 @@
  */
 package com.whqt;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -41,15 +44,23 @@ public class KComplementaryTest {
      * Test of countComplimentary method, of class KComplementary.
      */
     @Test
-    public void testCountComplimentary() {
-        System.out.println("countComplimentary");
-        int K = 0;
-        int[] A = null;
-        int expResult = 0;
-        int result = KComplementary.countComplimentary(K, A);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testFindComplimentary() {
+        System.out.println("FindComplimentary");
+        Integer K = 10;
+        Integer[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        Map<Integer, Integer> C = new HashMap<>( );
+        
+        C.put(A[0], A[8]);
+        C.put(A[1], A[7]);
+        C.put(A[2], A[6]);
+        C.put(A[3], A[5]);
+        C.put(A[5], A[3]);
+        C.put(A[6], A[2]);
+        C.put(A[7], A[1]);
+        C.put(A[8], A[0]);
+        
+        Map<Integer, Integer> result = KComplementary.findComplimentary(K, A);
+        assertEquals("wrong result! expected: "+C.toString()+" but was: "+result.toString(), C, result);
     }
     
 }
